d60465a19b4cf430b8db539e4c65ea0c0e3f4ec7
Vensim <39244927+Vensim@users.noreply.github.com>
2022-03-13 19:45:04 +0000

    Added analytics class to target specific cars

--- src/TrafficSim/model.py
+++ src/TrafficSim/model.py
@@ -4,6 +4,37 @@ import random
 import pyglet
 from pyglet import shapes
 
+
+class Analytics:
+    def __init__(self, batch):
+        self.batch = batch
+        self.velocity_meter = pyglet.text.Label('Velocity : {}'.format(1),
+                                                font_size=10,
+                                                x=width / 2,
+                                                y=height / 7,
+                                                anchor_x='center',
+                                                anchor_y='center', batch=self.batch)
+        self.distance_meter = pyglet.text.Label('Distance : {}'.format(1),
+                                                font_size=10,
+                                                x=width / 2,
+                                                y=height / 7 - 11,
+                                                anchor_x='center',
+                                                anchor_y='center', batch=self.batch)
+
+        self.acceleration_meter = pyglet.text.Label('Acceleration : {}'.format(1),
+                                                    font_size=10,
+                                                    x=width / 2,
+                                                    y=height / 7 - 22,
+                                                    anchor_x='center',
+                                                    anchor_y='center', batch=self.batch)
+
+    def update(self, Car):
+        self.velocity_meter.text = ("Velocity : {:.2f}".format(Car.velocity))
+        self.distance_meter.text = ("Distance : {:.2f}".format(Car.distance))
+        self.acceleration_meter.text = ("Acceleration : {:.2f}".format(Car.acceleration))
+        pass
+
+
 class Car:
     def __init__(self, renderer, acceleration=1, max_speed=1, velocity=0.4, number=0):
         self.number = number
@@ -51,38 +82,21 @@ class Window(pyglet.window.Window):
         self.width = width
         self.height = height
         self.distance_difference = 30
-        # self.velocity_meter = pyglet.text.Label('Velocity : {}'.format(int(self.velocity)),
-        #                                         font_size=10,
-        #                                         x=self.width / 2,
-        #                                         y=self.height / 7,
-        #                                         anchor_x='center',
-        #                                         anchor_y='center', batch=self.batch)
-        # self.distance_meter = pyglet.text.Label('Distance : {}'.format(int(self.distance)),
-        #                                         font_size=10,
-        #                                         x=self.width / 2,
-        #                                         y=self.height / 7 - 11,
-        #                                         anchor_x='center',
-        #                                         anchor_y='center', batch=self.batch)
-        #
-        # self.acceleration_meter = pyglet.text.Label('Acceleration : {}'.format(int(self.distance)),
-        #                                             font_size=10,
-        #                                             x=self.width / 2,
-        #                                             y=self.height / 7 - 22,
-        #                                             anchor_x='center',
-        #                                             anchor_y='center', batch=self.batch)
 
         self.OuterRim = shapes.Circle(width / 2, height / 2, height / 3, color=(255, 225, 255), batch=self.batch)
         self.InnerRim = shapes.Circle(width / 2, height / 2, height / 3 - 2, color=(0, 0, 0), batch=self.batch)
 
         self.Lines = []
         self.line = shapes.Line(251, 0, 0, 480, width=1, color=(225, 20, 20), batch=self.batch)
-        # Ideal oval shape has axis ratio of 2/1, minor/major.
-        # self.track = shapes.Ellipse(width/2, height/2, 300, 150, color=(122, 133, 122), batch=self.batch, group=None)
+
         self.Cars = []
 
-        for i in range(4):
+        for i in range(10):
             self.Cars.append(Car(self.batch, number=i))
             # self.Lines.append()
+
+        self.CarInfo = Analytics(self.batch)
+
 
     def on_draw(self):
         self.clear()
@@ -97,14 +111,14 @@ class Window(pyglet.window.Window):
         for i in range(3):
             distance_diff = self.Cars[i].distance - self.Cars[i + 1].distance
             x1, y1 = self.Cars[i].current_position
-            x2, y2 = self.Cars[i+1].current_position
+            x2, y2 = self.Cars[i + 1].current_position
             self.line.position = (x1, y1, x2, y2)
             if distance_diff > self.distance_difference:
                 self.Cars[i].velocity = (self.Cars[i + 1].velocity * 1.1)
             if distance_diff < self.distance_difference:
                 self.Cars[i].velocity = self.Cars[i + 1].velocity * 0.91
 
-        #
+        self.CarInfo.update(self.Cars[0])
         # for i in range(34):
         #     ds = self.Cars[i].distance - self.Cars[i+1].distance
         #     print(ds)
@@ -113,18 +127,11 @@ class Window(pyglet.window.Window):
         #     if ds < self.distance_difference:
         #         self.Cars[i].velocity =- 3
 
-        # Todo : 2 ways to measure distance between 2 cars
-        # Todo : Compare between 2 distances travelled, or find the distance
-        # self.velocity_meter.text = ("Velocity : {:.2f}".format(self.velocity))
-
         # Todo : Update meters
         # self.distance_step = self.velocity * delta_time
         # self.distance += self.distance_step
         # print("Speed({}) * Time({}) = Distance ({})".format(self.velocity, delta_time, self.distance_step))
-        # self.distance_meter.text = ("Distance : {:.2f}".format(self.distance))
-        # self.old_position = self.new_position
-        #
-        # self.acceleration_meter.text = ("Acceleration : {:.2f}".format(self.acceleration))
+
 
 
 # Distance is speed over time: s = v/t
